1ae0a742fded2183f4578be3b6b97c0c6b27f424
Asmaamanaa <manaaasmaa0@gmail.com>
2024-05-15 20:27:48 +0200

    added tas1

---
+++ tas1.py
@@ -0,0 +1,101 @@
+#Create a Boolean variable named x
+#x = 5
+#if x > 5:
+#    print('False')
+#else:
+#    print('True')
+
+#Create an integer variable named y 
+#y = 28
+#input('enter your age: ')
+#print(input('enter your age: '),input('age is 28'))
+
+#Create a string variable names s
+#name = ('s')
+#print("name")
+
+#Convert the int variable to float
+#num1 = float(input('enter num1'))
+#num2 = float(input('enter num2'))
+
+#Can we convert the str to int ? yes
+
+#Create a list of numbers from 1 to 5
+#list[1,2,3,4,5]
+
+#Create a tuple from 10 to 15
+#num = (10,11,12,13,14,15)
+
+#Convert the list to tuple
+#list : ["omar","nada"]
+#tuple : ("omar","nada")
+
+#Create a dict of 3 values
+#employee = {
+#   'name'  : 'omar',
+#   'age'   : 28 ,
+#   'salary': 246456
+#}
+
+#Can we use semi colon ; with python? yes
+
+#Python is interpreted or compiled ? interpreted
+
+#What is the differences between low level & high level
+#High-levellanguages do not depend on machines ,
+#low level languages are machine-dependent and thus very difficult to understand by a normal user
+
+#What is the differences between = , ==
+#(=) : Ex:
+#name = 'omar'
+#(==): equal
+#Ex : x == 5
+
+#What do we mean by using !=
+#not equal
+
+#What is the operator precedence
+#From up to down , From left to right (*,/) befor (+,-)
+
+#Create a variable x with value of 10
+x = 10
+print(x)
+
+#Increase x value by 15 using assignment operators
+x = 1
+y = 15
+print(1+15)
+
+#Divide the x value by 5 using assignment operators
+x = 20
+print(x/5)
+
+#Multiply the x value by 10 using assignment operator
+x = 2
+print(x*10)
+
+#Get module of x on 3 using assignment operators
+x = 16
+print(16%3)
+
+#Using python print the module of 11 to 4
+print(11%4)
+
+#Print the exponent of 2,3
+print(2**3)
+
+#Divide 11 by 3 using python division
+print(11/3)
+
+#Can we divide 11 by 3 and get an integer number ?
+#yes , by module
+
+#Check if 10 is bigger than 15 or not
+#no
+
+#If 10 is not bigger than15 print x is smaller than 15
+if 10 < 15 :
+    print('x')
+
+else :
+    print('y') 
